2579a2bb79986391b1c699f8df967cdbbaeb48c7
chenjie <873328316@qq.com>
2019-04-09 15:49:51 +0800

    spring_cloud_demo

---
+++ Eureka/serice-feign/src/main/java/com/springboot/feign/HiController.java
@@ -0,0 +1,22 @@
+package com.springboot.feign;
+
+import com.springboot.feign.SchedualServiceHi;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+public class HiController {
+    //编译器报错，无视。 因为这个Bean是在程序启动的时候注入的，编译器感知不到，所以报错。
+    @Autowired
+    SchedualServiceHi schedualServiceHi;
+
+    @GetMapping(value = "/hi")
+    public String sayHi(@RequestParam String name) {
+        return "from feign"+schedualServiceHi.sayHiFromClientOne( name );
+    }
+
+}
